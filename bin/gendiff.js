421109db6e19208d843b788d882b4effcbbc8359
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-03-18 13:35:35 +0500

    add arguments and option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import commander from 'commander';
 
 commander
   .version('1.0.0')
+  .arguments('<filepath1>,<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 commander.parse(process.argv);
