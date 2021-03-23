e18d0f0ff96c90d4f4faff20dc757e176d8c5249
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-03-23 16:44:35 +0500

    do make lint --fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import commander from 'commander';
 import getDiff from '../src/getDiff.js';
-//const getDiff = require('../src/getDiff')
+// const getDiff = require('../src/getDiff')
 
 commander
   .version('1.0.0')
@@ -9,7 +9,7 @@ commander
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
-  .action((filepath1,filepath2) => {
-    console.log(getDiff(filepath1,filepath2));
-  })
+  .action((filepath1, filepath2) => {
+    console.log(getDiff(filepath1, filepath2));
+  });
 commander.parse(process.argv);
--- src/getDiff.js
+++ src/getDiff.js
@@ -15,19 +15,19 @@ const parseObjToArr = (operator, obj) => Object.keys(obj)
 const compare = (arr1, arr2) => {
   const intersection = _
     .intersectionWith(arr1, arr2, (a, b) => a.prop === b.prop && a.value === b.value)
-    .map((el) => ({ ...el, oper: " "}));
+    .map((el) => ({ ...el, oper: ' ' }));
   const modifedArr1 = _.xorBy(arr1, intersection, 'prop');
   const modifedArr2 = _.xorBy(arr2, intersection, 'prop');
-  const result = _.sortBy([...modifedArr1, ...modifedArr2, ...intersection], (el) => el.prop)
-  return result
+  const result = _.sortBy([...modifedArr1, ...modifedArr2, ...intersection], (el) => el.prop);
+  return result;
 };
 
 const getSentence = (arr) => {
   const firstStr = '{\n';
   const lastStr = '}';
-  const bodyAnswer = arr.map(({ oper, prop, value}) => `  ${oper} ${prop}: ${value}\n`)
-  return [firstStr, ...bodyAnswer, lastStr].join('')
-}
+  const bodyAnswer = arr.map(({ oper, prop, value }) => `  ${oper} ${prop}: ${value}\n`);
+  return [firstStr, ...bodyAnswer, lastStr].join('');
+};
 
 export default (filepatch1, filepatch2) => {
   if (!filepatch1 || !filepatch2) return 'Not attribut';
