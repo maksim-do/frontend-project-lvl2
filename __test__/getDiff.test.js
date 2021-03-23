a00dd3a2e6e34ef6bd973e1fd3cfb2894e7fe3e7
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-03-23 16:38:30 +0500

    modifed dirname fixture

--- __test__/getDiff.test.js
+++ __test__/getDiff.test.js
@@ -1,15 +1,15 @@
-import getDiff from '../src/getDiff'
+import getDiff from '../src/getDiff';
 
 test('get diff json', () => {
-  const result = getDiff('./__test__/fixture/1.json', './__test__/fixture/2.json')
+  const result = getDiff('./__test__/__fixture__/1.json', './__test__/__fixture__/2.json');
   expect(result).toBe(
-`{
+    `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}`
+}`,
   );
 });
