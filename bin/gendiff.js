da0badeb65685a7a0aed46570ccb3b83b67217a1
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-03-23 14:05:38 +0500

    import diff function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import commander from 'commander';
-// const commander = require('commander')
+import getDiff from '../src/getDiff.js';
+//const getDiff = require('../src/getDiff')
 
 commander
   .version('1.0.0')
-  .arguments('<filepath1>,<filepath2>')
+  .arguments('[filepath1] [filepath2]')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1,filepath2) => {
+    console.log(getDiff(filepath1,filepath2));
+  })
 commander.parse(process.argv);
