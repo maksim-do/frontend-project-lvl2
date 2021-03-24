8d5bae6f2c213b3dd11808e0c99996eac2735083
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-03-24 15:08:28 +0500

    add test yml

--- __test__/getDiff.test.js
+++ __test__/getDiff.test.js
@@ -1,15 +1,20 @@
 import getDiff from '../src/getDiff';
 
-test('get diff json', () => {
-  const result = getDiff('./__test__/__fixture__/1.json', './__test__/__fixture__/2.json');
-  expect(result).toBe(
-    `{
+const result1 = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
   - timeout: 50
   + timeout: 20
   + verbose: true
-}`,
-  );
+}`;
+
+test('get diff json', () => {
+  const result = getDiff('./__test__/__fixture__/1.json', './__test__/__fixture__/2.json');
+  expect(result).toBe(result1);
+});
+
+test('get diff yml', () => {
+  const result = getDiff('./__test__/__fixture__/1.yml', './__test__/__fixture__/2.yml');
+  expect(result).toBe(result1);
 });
